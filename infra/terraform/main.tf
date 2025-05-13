afb9d0f884fb36a1711c2f9f2169141933755823
alberto <25645668+alberto7088@users.noreply.github.com>
2025-05-13 10:06:58 +0100

    Added terrafrom beta for vector compatibility

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -2,8 +2,14 @@ terraform {
   required_version = ">= 1.0.0"
 
   required_providers {
-    google      = { source = "hashicorp/google"      version = "~> 5.0" },
-    google-beta = { source = "hashicorp/google-beta" version = "~> 5.0" }
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 5.0"
+    }
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      version = "~> 5.0"
+    }
   }
 
   backend "gcs" {
