c2771d0033e10f40c171aad73d83d438fc7e0da1
alberto <25645668+alberto7088@users.noreply.github.com>
2025-05-13 15:10:13 +0100

    Removed vector indexing

--- infra/terraform/modules/bigquery/main.tf
+++ infra/terraform/modules/bigquery/main.tf
@@ -5,19 +5,4 @@ resource "google_bigquery_table" "this" {
   dataset_id = var.dataset_id
   table_id   = var.table_id
   schema = file(var.schema_file)
-
-  dynamic "vector_search_spec" {
-    for_each = var.vector_search_enabled ? [1] : []
-    content {
-      type = "VECTOR_INDEX"
-
-      # Specifies which column contains the vector data
-      columns = [var.embedding_column_name]
-
-      vector_search_dimensions {
-        dimension = var.vector_dimension
-      }
-      distance_type = "COSINE"
-    }
-  }
 }
